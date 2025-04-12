59d4d8f3bfe57f12acfef71239aba5a0b5963770
Dwika Ananta <dwika.ananta.wiratama@gmail.com>
2025-04-12 16:23:43 +0800

    update package json

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth";
 import { AdminLayout } from "@/layouts/admin";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/");
+  }
+
   return <AdminLayout>{children}</AdminLayout>;
 };
 
